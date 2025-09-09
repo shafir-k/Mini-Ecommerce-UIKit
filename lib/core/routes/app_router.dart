c3cea555bf267056d92a5bf4399f8fb291fdbd49
Shafir K <mohamedshafeer21@gmail.com>
2025-09-09 16:50:29 +0530

    AutoRoute and Riverpod Packages Added, Buld runner added

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => RouteType.cupertino();
+
+  @override
+  List<AutoRoute> get routes => [
+    /// routes go here
+  ];
+}
